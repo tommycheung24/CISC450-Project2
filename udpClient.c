a27f9b891d670e5d2b1b332ae7cb40c99f7fea36
Tommy <tcheung@udel.edu>
2021-05-03 17:43:48 -0400

    Start inital interaction
    Still having problems with server recieving it

---
+++ udpClient.c
@@ -0,0 +1,141 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+
+void storeText(int socket);
+unsigned char* createHeader(unsigned short count, unsigned short seq);
+unsigned char* combineText(unsigned char* header, unsigned char* data);
+unsigned char* getHeader(unsigned char* response);
+
+int main(){
+
+	int clientSock;
+	struct sockaddr_in serverAddress;
+
+	unsigned char clientMessage[256], header[4];
+
+	//gets the name of the file from user
+	printf("Enter file name: ");
+	scanf("%s", &clientMessage);
+	
+	clientSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);  //creates a socket
+
+	if(clientSock< 0){
+		printf("socket() failed");
+		return 0;
+	}
+
+	serverAddress.sin_family = AF_INET;
+	serverAddress.sin_port = htons(8003);
+	serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+	/*
+	if(bind(clientSock, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) < 0){
+		printf("bind() failed");
+	}
+	*/
+
+	strcpy(header, createHeader((unsigned short) strlen(clientMessage), (unsigned short)0));
+	
+	unsigned char* text = combineText(header, clientMessage);
+
+	sendto(clientSock, text, strlen(text), 0,(struct sockaddr*) &serverAddress, sizeof(serverAddress));
+
+	printf("Strlen: %ld", strlen())
+	printf("Message Sent\n");
+	
+	free(text);
+	
+	//store incoming data for file
+	//storeText(clientSock);
+	close(clientSock);
+	return 0;
+}
+
+unsigned char* getHeader(unsigned char* response){
+	unsigned char header[4];
+
+	strncpy(header, response, 4);
+
+	unsigned char* returnHeader = malloc(strlen(header)+1);
+	strcpy(returnHeader, header);
+
+	return returnHeader;
+}
+
+void storeText(int socket){
+	/*
+	unsigned char responce[84];
+
+	int totalCount = 0;
+	int totalPacket = 0;
+
+	FILE * file;
+	file = fopen("out.txt", "w"); //write to file out.txt
+
+	int headerCount;
+	int dataCount;
+
+	while(1){
+		
+		//reassemble the char array into two shorts(count and sequence number)
+		//unsigned short count = header[0] + (header[1] << 8);
+		//unsigned short seq = header[2] + (header[3] << 8);
+
+		//clears out the char array
+		bzero(responce, 84);
+		//recieves the data
+		dataCount = recv(socket, responce, sizeof(responce), 0);
+
+		//if the length of the data is 0 and count is zero, then it's the end of transmission
+		if(!strlen(serverResponce) && (count == 0)){
+			printf("End of Transmission Packet with sequence number %d received with %d data bytes\n", seq, count);
+			break;
+		}
+		//puts the data into the file
+		fputs(serverResponce, file);
+		//update value
+		totalCount += count;
+		++totalPacket;
+		printf("Packet %d received with %d data bytes\n", seq, count);		
+		
+		//signal to move on
+		send(socket, "", sizeof(""), 0);
+
+	}
+	//close file
+	fclose(file);
+
+	printf("Number of data packets received: %d\n", totalPacket);
+	printf("Number of data bytes received: %d\n", totalCount);
+	*/
+}
+
+unsigned char* combineText(unsigned char* header, unsigned char* data){
+	unsigned char* combine = malloc(strlen(header) + strlen(data) + 1);
+
+	strcpy(combine, header);
+	strcat(combine+4, data);
+
+	return combine;
+}
+
+unsigned char* createHeader(unsigned short count, unsigned short seq){
+	unsigned char header[4];
+
+	//dissambles count and sequence number into a 4 bytes char array
+	header[0] = count;
+	header[1] = count >> 8;
+	header[2] = seq;
+	header[3] = seq >> 8;
+
+	unsigned char * headerString = malloc(sizeof(header) + 1);
+	strcpy(headerString, header);
+
+	return headerString;
+}
---
+++ udpServer.c
@@ -0,0 +1,149 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+void sendText(int socket,char* textName);
+unsigned char* createHeader(unsigned short count, unsigned short sequenceNumber);
+unsigned char* getHeader(unsigned char* response);
+unsigned char* getMessage(unsigned char* response);
+
+int main(){
+
+	unsigned char response[256];
+
+	int serverSock;
+	struct sockaddr_in servAddress, clientAddress;
+	socklen_t addr_size = sizeof(clientAddress);
+
+	serverSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if(serverSock< 0){
+		printf("socket() failed");
+		return 0;
+	}
+
+	servAddress.sin_family = AF_INET;
+	servAddress.sin_port = htons(8003);
+	servAddress.sin_addr.s_addr = INADDR_ANY;
+
+	//binds the socket with the server Address
+	if(bind(serverSock, (struct sockaddr *) &servAddress, sizeof(servAddress)) < 0){
+		printf("bind() failed");
+		return 0;
+	}
+
+	if(recvfrom(serverSock, response, sizeof(response), 0,(struct sockaddr*) &clientAddress, &addr_size) <0 ){
+		printf("header recv() failed\n");
+		return 0;
+	}
+
+	printf("Message recieved\n");
+	printf("Size of message: %ld\n", strlen(response));
+	unsigned char header[4];
+	strcpy(header, getHeader(response));
+	unsigned short seq = header[0] + (header[1] << 8);
+	unsigned short count = header[2] + (header[3] << 8);
+
+	printf("%d %d\n", seq, count);
+
+	printf("%s\n", getMessage(response));
+
+	//sendText(clientSock, clientResponce);
+
+	close(serverSock);
+
+	return 0;
+}
+unsigned char* getHeader(unsigned char* response){
+	unsigned char header[4];
+
+	strncpy(header, response, 4);
+
+	unsigned char* returnHeader = malloc(strlen(header)+1);
+	strcpy(returnHeader, header);
+
+	return returnHeader;
+}
+
+unsigned char* getMessage(unsigned char* response){
+
+	printf("response: %ld\n", strlen(response));
+	unsigned char *newResponce = malloc(strlen(response) + 1);
+
+	strcpy(newResponce, response);
+	strcat(newResponce, "\0");
+
+	printf("newResponce: %ld\n", strlen(newResponce));
+
+	return newResponce;
+}
+/*
+void sendText(int socket,char* textName){
+	
+	//line_buffer is the line in the file, confirm is the response from client 
+	char line_buffer[80], confirm[1];
+
+	FILE* file;
+	file= fopen(textName, "r"); // read file with name textName
+
+	unsigned short sequenceNumber = 1;
+	int totalCount = 0;
+
+	//gets a single line in the file and stores it in line_buffer
+	while(fgets(line_buffer, sizeof(line_buffer), file)){
+
+		//cuts the unused space away before sending the data
+		char newLine[strlen(line_buffer)];
+		strcpy(newLine, line_buffer);
+		
+		//gets the size in bytes of the new char array
+		unsigned short count = (unsigned short) sizeof(newLine);
+		
+		//sends the header with info, count and sequence number
+		sendHeader(socket, count, sequenceNumber, 0);
+		//recieves a confirmation from client so the server don't move on until the client is ready
+		recv(socket, confirm, sizeof(confirm), 0);
+		//sends the actually data after the server recieves confirmation
+		send(socket, newLine, sizeof(newLine), 0);
+
+		//add 1 to sequence number, add count to totalCount(total bytes send)
+		++sequenceNumber;
+		totalCount += count;
+
+		//waits again for confirmation to move on from client
+		recv(socket, confirm, sizeof(confirm), 0);
+	}
+
+	//since it's out of loop it means that there's no more lines to grab from file
+	//time to send the End of Transmission packet
+	sendHeader(socket, 0, sequenceNumber, 1);
+	//sends an empty data 
+	send(socket, "", sizeof(""), 0);
+
+	printf("Number of data packets transmitted: %d\n", sequenceNumber -1);
+	printf("Total number of bytes transmitted: %d\n", totalCount);
+
+	fclose(file);
+}
+*/
+
+unsigned char * createHeader(unsigned short count, unsigned short sequenceNumber){
+	unsigned char header[4];
+
+	//putting the two shorts into a 4 bytes char array 
+	header[0] = count;
+	header[1] = count >> 8;
+	header[2] = sequenceNumber;
+	header[3] = sequenceNumber >> 8;
+
+	unsigned char * headerString = malloc(sizeof(header) + 1);
+	strcpy(headerString, header);
+
+	return headerString;
+}
+
